5b83ac0333cbe28b86b461bcb043ff840ce64c0e
owati <mofiyinfoluwaowati@gmail.com>
2022-11-30 23:39:28 +0100

    AI fair sharp commit

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 //set up express app 
 const app = express();
 
 
-
+app.use(cors({
+    origin : ['*']
+}))
 // connect to mongodb
-mongoose.connect('mongodb://localhost/nodesgo');
+mongoose.connect(process.env.MONGO_URL);
 mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
