f420bc331cf3650ff8e3369922cbf9394e044eec
owati <mofiyinfoluwaowati@gmail.com>
2023-01-31 23:05:30 +0100

    added the state history

--- models/nodes.js
+++ models/nodes.js
@@ -39,10 +39,35 @@ const NodesSchema = new Schema({
     },
 });
 
+const NodeHistory = new Schema({
+    temp : String,
+    humidity : String,
+    bar_pressure : String,
+    vapor_pressure : String,
+    date : Date
+})
+
+const NodeHistoryStateSchema = new Schema({
+    node : {
+        type : String,
+        required : [true, 'The node field is required']
+    },
+    history :  {
+        type : [NodeHistory],
+        default : []
+    }
+
+})
+
+const TestSchema = new Schema({
+    value : String
+})
 const Nodes = mongoose.model('nodes', NodesSchema);
 const GeoLocations = mongoose.model('geo', GeoSchema);
+const Test = mongoose.model('test', TestSchema);
+const NodeHistoryState = mongoose.model('Node History', NodeHistoryStateSchema);
 
-module.exports = {Nodes, GeoLocations};
+module.exports = {Nodes, GeoLocations, Test, NodeHistoryState};
 
 function newFunction() {
     GeoSchema.nodes({ "loc": "2dsphere" });
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {Nodes, Geolocation} = require('../models/nodes');
+const {Nodes, Geolocation, NodeHistoryState} = require('../models/nodes');
 
 //get a list of nodes from the db
 router.get('/nodes', function(req,res,next){
@@ -38,6 +38,7 @@ router.get('/nodes', function(req,res,next){
 
 //add a new node to the db
 router.post('/nodes', async function(req,res,next){
+    console.log('Test the ')
     try {
         const node = await Nodes.create(req.body);
         console.log(node);
@@ -54,8 +55,53 @@ router.post('/nodes', async function(req,res,next){
          
 });
 
+router.put('/nodes/history/:id', async function (req, res) {
+    console.log('Updating the Node history');
+    try {
+        const {id} = req.params;
+        const {body} = req;
+
+        const node = await Nodes.findById(id);
+
+        if (!node)
+            return res.status(422)
+                      .send({message : 'The id sent is invalid'});
+                      
+        let [nodehistory] = await  NodeHistoryState.find({node : id});
+        if (!nodehistory)
+            nodehistory = await NodeHistoryState.create({node : id});
+        console.log(nodehistory, node, "=> node history")
+        if (nodehistory.history?.length < 10) {
+            nodehistory.history = [...nodehistory.history, {...body, date : Date.now()}];
+            await nodehistory.save();
+            return res.status(200)
+                        .send({
+                            message : 'The history is updated',
+                            data : nodehistory
+                        })
+        }
+
+        const history = [...nodehistory.history];
+        const randNum = Math.floor(Math.random() * 9) + 1;
+        history.splice(randNum, randNum);
+        history.push({...body, date : Date.now()})
+        nodehistory.history = history;
+        await nodehistory.save();
+
+        return res.status(200)
+                 .send({
+                    message : "The histoy is updated",
+                    data : nodehistory
+                 })
+        
+    } catch(e) {
+        console.log(e.message);
+    }
+})
+
 //update a node in the db
 router.put('/nodes/:id', function(req,res,next){
+    console.log('Someting came in from the derver')
     Nodes.findByIdAndUpdate({_id:req.params.id}, req.body).then(function(){
         Nodes.findOne({_id:req.params.id}).then(function(nodes){
             res.send(nodes);
@@ -71,4 +117,7 @@ router.delete('/nodes/:id', function(req,res,next){
     });
 });
 
+router.post('/test', async (req, res)=> {
+
+})
 module.exports = router;
